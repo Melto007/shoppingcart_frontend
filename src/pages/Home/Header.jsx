012a1db4b10435081396372116c1e3e634f3babd
Melto007 <meltosm8@gmail.com>
2024-03-30 08:39:27 +0530

    scroll bar color is altered

--- src/pages/Home/Header.jsx
+++ src/pages/Home/Header.jsx
@@ -31,12 +31,12 @@ function Header() {
                 {ProductContainer.length !== 0 && ProductContainer.map((item, idx) => (
                     <SwiperSlide className='flex px-2' key={idx}>
                         <div className='max-w-4xl mx-auto my-auto flex justify-center items-center'>
-                            <div className='w-full'>
+                            <div className='w-full ml-2'>
                                 <HeadingComponent
                                     content={item.content}
-                                    className="font-bold text-xl xmd:text-3xl xmd:mb-2 md:text-4xl"
+                                    className="font-bold text-xl xmd:text-3xl xmd:mb-2 md:text-4xl lg:text-5xl"
                                 />
-                                <div className='font-semibold text-sm xmd:text-md xmd:mb-1 md:text-lg'>
+                                <div className='font-semibold text-sm xmd:text-md xmd:mb-1 md:text-lg lg:text-xl'>
                                     <Paragraph
                                         content={item.title}
                                     />
@@ -46,12 +46,12 @@ function Header() {
                                 </div>
                                 <Paragraph
                                     content="Earn upto 10% off"
-                                    className="font-medium text-sm mb-2"
+                                    className="font-medium text-sm mb-2 lg:text-lg"
                                 />
                                 <ButtonComponent
                                     content={item.button}
                                     size="sm"
-                                    className="bg-slate text-zinc font-bold rounded-full"
+                                    className="bg-slate text-zinc font-bold rounded-full lg:p-3"
                                 />
                             </div>
                             <div className='w-full'>

af5979828c8696915055e4be99620c8b6de64c5a
Melto007 <meltosm8@gmail.com>
2024-04-04 10:13:55 +0530

    details product top header is created

--- src/pages/Product/DetailProduct.jsx
+++ src/pages/Product/DetailProduct.jsx
@@ -11,7 +11,7 @@ function DetailProduct() {
     return (
         <>
             <Container>
-                <div className="max-w-7xl m-auto">
+                <div className="max-w-6xl m-auto">
                     <div className="px-2 md:flex">
                         <div className="w-full">
                             <ImageComponent
@@ -19,20 +19,20 @@ function DetailProduct() {
                                 alt="product-1"
                             />
                         </div>
-                        <div className="flex flex-col gap-1 w-full">
+                        <div className="flex flex-col gap-1 w-full md:my-auto">
                             <HeadingComponent
                                 content="Samsung"
-                                className="text-xl font-bold"
+                                className="text-xl font-bold md:text-3xl"
                             />
 
                             <div className="flex items-center justify-between gap-2">
                                 <div className="flex gap-3 items-center">
                                     <HeadingComponent
                                         content="$14,000"
-                                        className="text-xl text-rosedark font-bold"
+                                        className="text-xl text-rosedark font-bold md:text-4xl"
                                     />
                                 </div>
-                                <div className="flex items-center">
+                                <div className="flex items-center md:text-xl">
                                     <Icons name="star" />
                                     <Icons name="star" />
                                     <Icons name="star" />
@@ -42,28 +42,61 @@ function DetailProduct() {
                             </div>
 
                             <div className="flex flex-col gap-2 mt-2">
-                                <ButtonComponent
-                                    size="sm"
-                                    radius="none"
-                                    content="Order"
-                                    color="danger"
-                                    className="w-full font-bold"
-                                />
-                                <ButtonComponent
-                                    size="sm"
-                                    radius="none"
-                                    content="Add to cart"
-                                    className="w-full font-bold bg-slate text-zinc"
-                                />
+                                <div className="flex flex-col gap-2 md:flex-row">
+                                    <ButtonComponent
+                                            size="sm"
+                                            radius="full"
+                                            content="Order"
+                                            color="danger"
+                                            className="w-full font-bold"
+                                        />
+                                        <ButtonComponent
+                                            size="sm"
+                                            radius="full"
+                                            content="Add to cart"
+                                            className="w-full font-bold bg-slate text-zinc"
+                                        />
+                                </div>
 
                                 <div className="mt-2">
-                                    <HeadingComponent
-                                        content="Description"
-                                        className="font-bold text-lg"
-                                    />
-                                    <Paragraph
-                                        content=" Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse itaque nisi qui? Odio, esse voluptatum. Doloribus exercitationem rem, ipsa ullam sed, atque obcaecati optio sit sequi in sunt harum? Dolores magni harum dolorem temporibus fugit placeat aut omnis quaerat iste praesentium ad recusandae aliquam cupiditate eos fugiat similique, reprehenderit quas perferendis cum, assumenda, molestias vero alias! Necessitatibus at saepe doloremque facilis, suscipit cumque sed quos earum quibusdam possimus dolores adipisci ipsam delectus qui, id quam aliquam. Distinctio nam aliquam soluta sapiente! Ex atque minima quos fugiat repellat sint eaque quidem cum non, accusamus odit sequi adipisci nihil autem veniam nostrum."
-                                    />
+                                    <div className="flex gap-4">
+                                        <div className="md:text-lg">
+                                            <Paragraph
+                                                content="Brand Name"
+                                                className="font-bold mb-2"
+                                            />
+                                            <Paragraph
+                                                content="Model Number"
+                                                className="font-bold mb-2"
+                                            />
+                                            <Paragraph
+                                                content="Release Date"
+                                                className="font-bold mb-2"
+                                            />
+                                            <Paragraph
+                                                content="Color"
+                                                className="font-bold mb-2"
+                                            />
+                                        </div>
+                                        <div>
+                                            <Paragraph
+                                                className="mb-2"
+                                                content="Samsung"
+                                            />
+                                            <Paragraph
+                                                className="mb-2"
+                                                content="2001"
+                                            />
+                                            <Paragraph
+                                                className="mb-2"
+                                                content="22-03-2020"
+                                            />
+                                            <Paragraph
+                                                className="mb-2"
+                                                content="black"
+                                            />
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>

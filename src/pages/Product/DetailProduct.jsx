1d1eaf37ee3616f6d1d0ea2b7b664d060bc9e39a
Melto007 <meltosm8@gmail.com>
2024-04-04 10:36:17 +0530

    product related item container is completed

--- src/pages/Product/DetailProduct.jsx
+++ src/pages/Product/DetailProduct.jsx
@@ -1,13 +1,36 @@
+import {
+    useState,
+    useRef
+} from 'react'
 import Container from "../../components/Container"
 import ImageComponent from "../../components/ImageComponent"
 import HeadingComponent from '../../components/HeadingComponent'
 import Icons from "../../components/Icons"
 import Paragraph from '../../components/Paragraph'
 import ButtonComponent from '../../components/ButtonComponent'
+import CardHeaderComponent from "../../components/CardHeaderComponent"
+import {
+    Card,
+    CardBody,
+    CardHeader,
+    CardFooter
+} from '@nextui-org/react'
+import { Swiper, SwiperSlide } from 'swiper/react'
+
+import 'swiper/css'
+import 'swiper/css/free-mode'
+import 'swiper/css/pagination'
+
+import { Autoplay, FreeMode, Pagination, Virtual, Navigation } from 'swiper/modules'
+
+import { cardContainer } from '../Home/ProductContainer'
 
 import product01 from '../../../public/product01.png'
 
 function DetailProduct() {
+    const sliderRef = useRef()
+    const [slidesPerView, setSlidesPerView] = useState(1)
+
     return (
         <>
             <Container>
@@ -102,6 +125,73 @@ function DetailProduct() {
                         </div>
 
                     </div>
+                </div>
+
+                <CardHeaderComponent
+                        sliderRef={sliderRef}
+                        setSlidesPerView={setSlidesPerView}
+                        content="Products related to this item"
+                    />
+
+                <div className='px-2 max-w-7xl mx-auto'>
+                    <Swiper
+                        ref={sliderRef}
+                        autoplay={{
+                            delay: 3500,
+                            disableOnInteraction: false,
+                        }}
+                        modules={[Autoplay, FreeMode, Virtual, Navigation, Pagination]}
+                        slidesPerView={slidesPerView}
+                        spaceBetween={30}
+                        virtual
+                        className='flex flex-row p-0 gap-2'
+                    >
+                        {cardContainer.map((item, idx) => (
+                            <SwiperSlide key={idx} className="!mx-3">
+                                <Card shadow="md" isPressable className='mb-4 w-full'>
+                                    <CardHeader>
+                                        <ImageComponent
+                                            src={item.image}
+                                            alt={item.name}
+                                        />
+                                    </CardHeader>
+                                    <CardBody className='text-center p-0'>
+                                        <HeadingComponent
+                                            content={item.name}
+                                            className="xxs:text-md md:text-lg font-bold"
+                                        />
+                                    </CardBody>
+                                    <CardFooter className='flex flex-col'>
+                                        <div className='flex justify-between items-center gap-2 mb-4 md:gap-4 lg:gap-8'>
+                                            <span className='text-[0.8rem] text-rosedark font-bold xxs:text-md md:text-lg'>{item.price}</span>
+                                            <span className='flex text-[0.8rem] xxs:text-md md:text-lg'>
+                                                <Icons name="star" />
+                                                <Icons name="star" />
+                                                <Icons name="star" />
+                                                <Icons name="star" />
+                                                <Icons name="star" />
+                                            </span>
+                                        </div>
+                                        <div className='flex flex-col gap-2 w-full'>
+                                            <ButtonComponent
+                                                content="Shop now"
+                                                size="sm"
+                                                color="danger"
+                                                className="text-[0.7rem] bg-slate rounded-none font-bold"
+                                            />
+                                            <ButtonComponent
+                                                content="Add cart"
+                                                size="sm"
+                                                variant="bordered"
+                                                color="danger"
+                                                className="text-[0.7rem] rounded-none font-bold"
+                                            />
+                                        </div>
+                                    </CardFooter>
+                                </Card>
+                            </SwiperSlide>
+                        ))}
+                    </Swiper>
                 </div>
             </Container>
         </>
